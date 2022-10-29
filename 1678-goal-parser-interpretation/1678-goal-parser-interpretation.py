471a1826e9e7abbcd105fb411a562649091992cc
Dan Card <63565005+dancard32@users.noreply.github.com>
2022-10-28 20:50:03 -0400

    Time: 52 ms (60.73%), Space: 13.9 MB (55.80%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,6 +1,20 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        out = command.replace("()", "o")
-        out = out.replace("(al)", "al")
+        #out = command.replace("()", "o")
+        #out = out.replace("(al)", "al")
+        
+        
+        out = ""
+        k = 0
+        while k < len(command):
+            if command[k:k+2] == "()":
+                out += "o"
+                k += 2
+            elif command[k:k+4] == "(al)":
+                out += "al"
+                k += 4
+            else:
+                out += command[k]
+                k += 1
         
         return out
